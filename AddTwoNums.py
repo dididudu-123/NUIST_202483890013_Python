7a2643de618d76b4514520bf72314f1246eaac0c
dididudu-123 <3338125685@qq.com>
2025-03-18 19:39:29 +0800

    AddTwoNums.py

--- AddTwoNums.py
+++ AddTwoNums.py
@@ -8,3 +8,17 @@ b = 12
 res = a + b
 print(res)
 
+
+#Version2
+#Add Two Numbers in Python
+#Author: Shijia He
+#Using user input
+
+#taking user input
+a = input("First number: ")
+b = input("Second number: ")
+
+#converting inpt to float and adding
+res = float(a) + float(b)
+print(res)
+
